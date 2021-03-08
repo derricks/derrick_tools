18918e25b0498ea442f669239737e6de79f20636
Derrick Schneider <dschneider@fitbit.com>
2021-03-08 09:09:21 -0800

    Add a memory quiz for cranial nerves

---
+++ cmd/cranialnerves.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"math/rand"
+
+	"github.com/spf13/cobra"
+)
+
+// cranialnervesCmd represents the cranialnerves command
+var cranialnervesCmd = &cobra.Command{
+	Use:   "cranialnerves",
+	Short: "Quiz about the cranial nerves",
+	Run:   quizCranialNerves,
+}
+
+var cranialNerves = []string{
+	"olfactory",
+	"optic",
+	"oculomotor",
+	"trochlear",
+	"trigeminal",
+	"abducens",
+	"facial",
+	"auditory",
+	"glossopharyngeal",
+	"vagus",
+	"spinal accessory",
+	"hypoglossal",
+}
+
+type quizNerveFunc func([]string) promptAndResponse
+
+func quizCranialNerves(cmd *cobra.Command, args []string) {
+	nerveQuizzes := []quizNerveFunc{
+		quizCranialNerveByIndex,
+		quizCranialNerveIndexFromName,
+	}
+
+	quizFunction := nerveQuizzes[rand.Intn(len(nerveQuizzes))]
+	promptAndCheckResponse(quizFunction(cranialNerves))
+}
+
+func quizCranialNerveByIndex(nerves []string) promptAndResponse {
+	return quizStringAtIndexInList("cranial nerve", nerves)
+}
+
+func quizCranialNerveIndexFromName(nerves []string) promptAndResponse {
+	return quizIndexOfStringInList(nerves)
+}
+
+func init() {
+	memoryquizCmd.AddCommand(cranialnervesCmd)
+}
