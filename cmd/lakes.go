0e951da11d92802f49162c467430ea86245cc80f
Derrick Schneider <dfschneider@google.com>
2022-06-27 16:38:38 -0700

    Add bordering countries to lake quiz

--- cmd/lakes.go
+++ cmd/lakes.go
@@ -34,55 +34,56 @@ type lakeInfo struct {
 	sizeOrder int
 	name      string
 	isSaline  bool
+	countries []string
 }
 
 var lakes = []lakeInfo{
-	lakeInfo{1, "Caspian Sea", true},
-	lakeInfo{2, "Superior", false},
-	lakeInfo{3, "Victoria", false},
-	lakeInfo{4, "Huron", false},
-	lakeInfo{5, "Michigan", false},
-	lakeInfo{6, "Tanganyika", false},
-	lakeInfo{7, "Baikal", false},
-	lakeInfo{8, "Great Bear Lake", false},
-	lakeInfo{9, "Malawi", false},
-	lakeInfo{10, "Great Slave Lake", false},
-	lakeInfo{11, "Erie", false},
-	lakeInfo{12, "Winnipeg", false},
-	lakeInfo{13, "Ontario", false},
-	lakeInfo{14, "Ladoga", false},
-	lakeInfo{15, "Balkhash", true},
-	lakeInfo{16, "Bangweulu", false},
-	lakeInfo{17, "Vostok", false},
-	lakeInfo{18, "Onega", false},
-	lakeInfo{19, "Titicaca", false},
-	lakeInfo{20, "Nicaragua", false},
-	lakeInfo{21, "Athabasca", false},
-	lakeInfo{22, "Turkana", true},
-	lakeInfo{23, "Reindeer Lake", false},
-	lakeInfo{24, "Issyk-Kul", true},
-	lakeInfo{25, "Urmia", true},
-	lakeInfo{26, "Vanern", false},
-	lakeInfo{27, "Winnpegosis", false},
-	lakeInfo{28, "Albert", false},
-	lakeInfo{29, "Mweru", false},
-	lakeInfo{30, "Nettilling", false},
-	lakeInfo{31, "Nipigon", false},
-	lakeInfo{32, "Manitoba", false},
-	lakeInfo{33, "Taymyr", false},
-	lakeInfo{34, "Qinghai Lake", true},
-	lakeInfo{35, "Saimaa", false},
-	lakeInfo{36, "Lake of the Woods", false},
-	lakeInfo{37, "Khanka", false},
-	lakeInfo{38, "Sarygamyish", false},
-	lakeInfo{39, "Dubawnt", false},
-	lakeInfo{40, "Van", true},
-	lakeInfo{41, "Peipus", false},
-	lakeInfo{42, "Uvs", true},
-	lakeInfo{43, "Poyang", false},
-	lakeInfo{44, "Tana", false},
-	lakeInfo{45, "Amadjuak", false},
-	lakeInfo{46, "Melville", true},
+	lakeInfo{1, "Caspian Sea", true, []string{"Russia", "Kazakhstan", "Turkmenistan", "Iran", "Azerbaijan"}},
+	lakeInfo{2, "Superior", false, []string{"Canada", "United States"}},
+	lakeInfo{3, "Victoria", false, []string{"Kenya", "Tanzania", "Uganda"}},
+	lakeInfo{4, "Huron", false, []string{"Canada", "United States"}},
+	lakeInfo{5, "Michigan", false, []string{"United States"}},
+	lakeInfo{6, "Tanganyika", false, []string{"Burundi", "Zambia", "Democratic Republic of the Congo", "Tanzania"}},
+	lakeInfo{7, "Baikal", false, []string{"Russia"}},
+	lakeInfo{8, "Great Bear Lake", false, []string{"Canada"}},
+	lakeInfo{9, "Malawi", false, []string{"Mozambique", "Tanzania"}},
+	lakeInfo{10, "Great Slave Lake", false, []string{"Canada"}},
+	lakeInfo{11, "Erie", false, []string{"Canada", "United States"}},
+	lakeInfo{12, "Winnipeg", false, []string{"Canada"}},
+	lakeInfo{13, "Ontario", false, []string{"Canada", "United States"}},
+	lakeInfo{14, "Ladoga", false, []string{"Russia"}},
+	lakeInfo{15, "Balkhash", true, []string{"Kazakhstan"}},
+	lakeInfo{16, "Bangweulu", false, []string{"Zambia"}},
+	lakeInfo{17, "Vostok", false, []string{"Antarctica"}},
+	lakeInfo{18, "Onega", false, []string{"Russia"}},
+	lakeInfo{19, "Titicaca", false, []string{"Bolivia", "Peru"}},
+	lakeInfo{20, "Nicaragua", false, []string{"Nicaragua"}},
+	lakeInfo{21, "Athabasca", false, []string{"Canada"}},
+	lakeInfo{22, "Turkana", true, []string{"Kenya", "Ethiopia"}},
+	lakeInfo{23, "Reindeer Lake", false, []string{"Canada"}},
+	lakeInfo{24, "Issyk-Kul", true, []string{"Kyrgyzstan"}},
+	lakeInfo{25, "Urmia", true, []string{"Iran"}},
+	lakeInfo{26, "Vanern", false, []string{"Sweden"}},
+	lakeInfo{27, "Winnpegosis", false, []string{"Canada"}},
+	lakeInfo{28, "Albert", false, []string{"Uganda", "Democratic Republic of the Congo"}},
+	lakeInfo{29, "Mweru", false, []string{"Zambia"}},
+	lakeInfo{30, "Nettilling", false, []string{"Canada"}},
+	lakeInfo{31, "Nipigon", false, []string{"Canada"}},
+	lakeInfo{32, "Manitoba", false, []string{"Canada"}},
+	lakeInfo{33, "Taymyr", false, []string{"Russia"}},
+	lakeInfo{34, "Qinghai Lake", true, []string{"China"}},
+	lakeInfo{35, "Saimaa", false, []string{"Finland"}},
+	lakeInfo{36, "Lake of the Woods", false, []string{"Canada", "United States"}},
+	lakeInfo{37, "Khanka", false, []string{"China"}},
+	lakeInfo{38, "Sarygamyish", false, []string{"Uzbekistan", "Turkmenistan"}},
+	lakeInfo{39, "Dubawnt", false, []string{"Canada"}},
+	lakeInfo{40, "Van", true, []string{"Turkey"}},
+	lakeInfo{41, "Peipus", false, []string{"Russia", "Estonia"}},
+	lakeInfo{42, "Uvs", true, []string{"Mongolia"}},
+	lakeInfo{43, "Poyang", false, []string{"China"}},
+	lakeInfo{44, "Tana", false, []string{"Ethiopia"}},
+	lakeInfo{45, "Amadjuak", false, []string{"Canada"}},
+	lakeInfo{46, "Melville", true, []string{"Canada"}},
 }
 
 type lakeQuiz func([]lakeInfo) promptAndResponse
@@ -92,6 +93,7 @@ func quizLakes(cmd *cobra.Command, args []string) {
 		quizLakeBySizeRank,
 		quizSizeByLake,
 		quizLakeSalinity,
+		quizLakeInCountry,
 	}
 
 	quiz := quizzes[rand.Intn(len(quizzes))]
@@ -115,6 +117,13 @@ func quizSizeByLake(lakes []lakeInfo) promptAndResponse {
 func quizLakeSalinity(lakes []lakeInfo) promptAndResponse {
 	lake := randomLake(lakes)
 	return promptAndResponse{fmt.Sprintf("%s is a saline lake, true or false?", lake.name), strconv.FormatBool(lake.isSaline)}
+}
+
+func quizLakeInCountry(lakes []lakeInfo) promptAndResponse {
+	lake1 := randomLake(lakes)
+	country := lake1.countries[rand.Intn(len(lake1.countries))]
+	lake2 := randomLake(lakes)
+	return promptAndResponse{fmt.Sprintf("Lake %s touches %s, true or false?", lake2.name, country), strconv.FormatBool(isStringInSlice(country, lake2.countries))}
 }
 
 func init() {
