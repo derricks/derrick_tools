96c3fb679b930fd10bb3cb00da4ef12fe2c857d8
Derrick Schneider <dfschneider@google.com>
2023-01-13 15:31:11 -0800

    Add ULNA family to Spelling Bee

--- cmd/spelling_bee.go
+++ cmd/spelling_bee.go
@@ -78,6 +78,7 @@ var spellingBeeSets = [][]string{
 	{"PALL", "APPALL", "PAPAL", "PALAPA"},
 	{"VIAL", "AVAIL", "VILLA"},
 	{"ANVIL", "VILLAIN", "VANILLA", "VANILLIN"},
+	{"LUNA", "ULNA", "ANNUL", "ANNUAL"},
 }
 
 func quizSpellingBee(cmd *cobra.Command, args []string) {
