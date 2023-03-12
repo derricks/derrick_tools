b7df5e4114c1671a681d9b1427bb8e1c2d984645
Derrick Schneider <dfschneider@google.com>
2023-03-12 09:49:55 -0700

    Add a generic "quiz this field based on some other field" functionality
    Within the memory quiz subcommands, there is a common pattern of "for this type of thing X with a field value of Y, what is this other field value?" For instance, if a country struct has a name, a capital, a size rank, and a currency, we might want to quiz the name from the size or vice versa or the capital from the name or vice versa and so on.
    
    Right now, all the memory quizzes have to implement all those queries independently. One method for name from capital, one method for capital from name, and so on. This leads to a lot of boilerplate but also means those need to actually be coded.
    
    This new functionality lets a struct be annotated such that reflection code can generate all the possible queries.
    
    Example
    type country struct {
      sizeRank int `crossquery:"all"`
      name string `crossquery:"all"`
      capital string `crossquery:"all"`
      currency string `crossquery:"guess"`
    }
    
    This means that the code could generate quizzes such as name from size rank, size rank from name, capital from name, and so on. Flagging a field as "guess" means that it will only show up as something you need to guess. It doesn't make sense to ask which countries uses "franc" as a currency because there are many possible answers. Flagging it as "guess" means you'll only ever be asked to name the currency for a given country, size, capital, etc. and not the reverse. (There is an equivalent "given" for completeness sake)

---
+++ cmd/crossquery.go
@@ -0,0 +1,91 @@
+/*
+Copyright © 2022 Derrick Schneider derrick.schneider@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"strconv"
+)
+
+// Tools for making "cross queries" on objects used by memory quiz.
+// For instance, if a country struct has a number, a name, and a currency,
+// we should be able to automatically construct memory quizzes that ask about name from size,
+// size from name, currency from name, currency from size, and so on.
+// example struct annotation
+// type country {
+//   name string `crossquery:"all"`
+//   currency string `crossquery:"guess"` -- because so many countries use the same currency name, this can't be a given "What country has a currency of franc"
+//   capital string `crossquery:""`
+// }
+// currently only supports strings and ints
+
+func constructCrossQuery(entityType string, entity interface{}) promptAndResponse {
+	// the fields we could use as the "given" in the prompt. e.g., the country's name is Algeria
+	// these are fields annotated with crossquery:all or crossquery:given
+	givens := []reflect.StructField{}
+	// the fields we could use as things to guess in the prompt. e.g., the currency
+	// these are fields annotated with crossquery:all or crossquery:given
+	guesses := []reflect.StructField{}
+
+	reflectStruct := reflect.TypeOf(entity)
+	for i := 0; i < reflectStruct.NumField(); i++ {
+		field := reflectStruct.Field(i)
+		if crossQuery, ok := field.Tag.Lookup("crossquery"); ok {
+			if crossQuery == "" || crossQuery == "all" {
+				givens = append(givens, field)
+				guesses = append(guesses, field)
+			} else if crossQuery == "given" {
+				givens = append(givens, field)
+			} else if crossQuery == "guess" {
+				guesses = append(guesses, field)
+			} else {
+				// this is effectively a syntax error, so kill the program
+				panic(fmt.Sprintf("Invalid value for crossquery: %s", crossQuery))
+			}
+		}
+	}
+
+	reflectEntity := reflect.ValueOf(entity)
+
+	//get a given and figure out a non-equal guess
+	given := givens[rand.Intn(len(givens))]
+	guess := guesses[rand.Intn(len(guesses))]
+	for given.Name == guess.Name {
+		guess = guesses[rand.Intn(len(guesses))]
+	}
+
+	givenValueRaw := reflectEntity.FieldByName(given.Name)
+	guessValueRaw := reflectEntity.FieldByName(guess.Name)
+
+	// convert to strings if need be
+	givenValue := reflectValueToString(givenValueRaw)
+	guessValue := reflectValueToString(guessValueRaw)
+	return promptAndResponse{fmt.Sprintf("What is the %s of %s with %s of %v?", guess.Name, entityType, given.Name, givenValue), guessValue}
+}
+
+func reflectValueToString(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.String:
+		return v.String()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.Itoa(int(v.Int()))
+	default:
+		return v.String()
+	}
+}
---
+++ cmd/crossquery_test.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2022 Derrick Schneider derrick.schneider@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+// this can only produce one response string
+type crossQuery1 struct {
+	value1 string `crossquery:"given"`
+	value2 string `crossquery:"guess"`
+}
+
+func TestCrossQueryOneGivenOneGuess(test *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	s := crossQuery1{"abc", "def"}
+	result := constructCrossQuery("test1", s)
+	if result.prompt != "What is the value2 of test1 with value1 of abc?" {
+		test.Errorf("Incorrect prompt. Was \"%s\"", result.prompt)
+	}
+
+	if result.response != "def" {
+		test.Errorf("Incorrect response. Was \"%s\"", result.response)
+	}
+}
+
+type crossQuery2 struct {
+	v1 string `crossquery:""`
+	v2 int    `crossquery:"all"`
+}
+
+func TestCrossQueryAll(test *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	s := crossQuery2{"xyz", 34}
+	result := constructCrossQuery("test2", s)
+
+	// can be one of two
+	if result.prompt != "What is the v2 of test2 with v1 of xyz?" &&
+		result.prompt != "What is the v1 of test2 with v2 of 34?" {
+		test.Errorf("Invalid prompt. Was %s\n", result.prompt)
+	}
+
+	if result.response != "xyz" && result.response != "34" {
+		test.Errorf("Invalid response. Was \"%s\"\n", result.response)
+	}
+}
