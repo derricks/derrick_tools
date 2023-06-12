4293c6638974a7fd877845cd3df7458a188a437d
Derrick Schneider <dfschneider@google.com>
2023-06-12 15:22:24 -0700

    Correct help text for president quizzes

--- cmd/presidents.go
+++ cmd/presidents.go
@@ -27,14 +27,8 @@ import (
 // presidentsCmd represents the presidents command
 var presidentsCmd = &cobra.Command{
 	Use:   "presidents",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: quizPresidents,
+	Short: "Memory quizzes about presidents",
+	Run:   quizPresidents,
 }
 
 type president struct {
