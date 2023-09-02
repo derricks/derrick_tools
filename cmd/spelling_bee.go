7858f6bf31fa1974602f65d0a25eab8624dcce28
Derrick Schneider <dfschneider@fitbit.com>
2023-09-02 16:07:41 +0000

    Add MODEL family to spelling bee

--- cmd/spelling_bee.go
+++ cmd/spelling_bee.go
@@ -140,6 +140,7 @@ var spellingBeeSets = [][]string{
 	{"FLAT", "FATAL"},
 	{"BEING", "BEGGING", "BEGIN", "BINGEING", "BENIGN", "BINGE", "BEGINNING"},
 	{"TIDIED", "DIET", "TIDE", "TIED", "EDITED", "TIDED", "DIETED"},
+	{"MODEL", "LOOMED", "MODELED", "MOLDED"},
 }
 
 func quizSpellingBee(cmd *cobra.Command, args []string) {
